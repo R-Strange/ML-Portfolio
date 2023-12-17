67e36777a410b37ea81cd05495fd16b3a95dc609
Richard Strange <richardpstrange@outlook.com>
2023-12-17 21:15:23 +0000

    loss functions added

--- deep learning regression/pytorch_regression_data_prep.py
+++ deep learning regression/pytorch_regression_data_prep.py
@@ -3,11 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from torch import utils, from_numpy
 from torch.utils.data import DataLoader
 
-def get_diabetes_data(batch_size = 64):
+def get_diabetes_data(batch_size = 64, normalized=False, min_max=False):
     data_df = load_diabetes(as_frame=True).frame.astype(np.float32)
+
+    if normalized:
+        scaler = StandardScaler()
+        for column in data_df.columns:
+            data_df[column] = scaler.fit_transform(data_df[column].values.reshape(-1, 1))
+
+    if min_max:
+        scaler = MinMaxScaler()
+        for column in data_df.columns:
+            data_df[column] = scaler.fit_transform(data_df[column].values.reshape(-1, 1))
 
     train_df, test_df = train_test_split(data_df, test_size=0.3, random_state=42)
     train_df, val_df = train_test_split(train_df, test_size=0.3, random_state=42)
